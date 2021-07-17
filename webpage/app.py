095240585857358ca228bd7b3bf6ba13dd7ab4dd
Quielo Gogez <67394751+Quielo@users.noreply.github.com>
2021-07-16 21:41:48 -0500

    Flask implemented

--- webpage/app.py
+++ webpage/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
